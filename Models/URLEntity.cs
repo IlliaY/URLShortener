6531886a85b63e142338d5e8014cd6faaa5770c3
Illia Yevtushevskyi <illia.yevtushevskyi@gmail.com>
2023-02-25 04:49:12 +0100

    Removed URLEntity

--- Models/URLEntity.cs
+++
@@ -1,9 +0,0 @@
-namespace URLShortener.Models;
-
-
-public class URLEntity
-{
-    public Guid Id { get; set; }
-    public string URL { get; set; } = String.Empty;
-    public string ShortURL { get; set; } = String.Empty;
-}
